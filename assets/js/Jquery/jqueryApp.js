690afd60edc5e004c479f6eccaadbe948485443c
andyRS <andy337@hotmail.es>
2021-12-21 12:19:54 -0400

    Update jqueryApp.js

--- assets/js/Jquery/jqueryApp.js
+++ assets/js/Jquery/jqueryApp.js
@@ -4,13 +4,22 @@ $(document).ready(function () {
     //Declarar variables y llamarlo del DOM
      var menuResponsivo = $(".menu-responsivo");
      var menu = $('#menu');
+
+     var cerarMenuResponsivo = $('.cerrar-menu-responsivo');
     
 
     //activar boton del menu responsivo con evento Click
-   menuResponsivo.click(function(){
+    menuResponsivo.click(function(){
        //menu.show('slow');
        menu.fadeToggle('slow');
-       menuResponsivo.hide('slow');
+       menuResponsivo.hide('fast'); 
+   })
+
+   cerarMenuResponsivo.click(()=>{
+        menu.hide('slow');
+
+        cerarMenuResponsivo.hide('fast');
+        menuResponsivo.show('slow');
    })
 
 
